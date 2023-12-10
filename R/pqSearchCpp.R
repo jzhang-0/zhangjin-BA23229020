a11a1ae46bcab9e13f66f27f2cd5c14c5cc97447
jzhang <jzhang__1999@outlook.com>
2023-12-10 10:45:23 +0800

    Update

--- R/pqSearchCpp.R
+++ R/pqSearchCpp.R
@@ -8,7 +8,10 @@
 #' @return 返回搜索结果的索引。
 #' @export
 #' @examples
-#' # 示例代码...
+#' data <- matrix(rnorm(1000), ncol = 100)
+#' compressed <- pqCompress(data, 10, 5)
+#' query <- rnorm(100)
+#' nearest_neightbors <- pqSearchCpp(query, compressed$compressed_data, compressed$centroids)
 pqSearchCpp <- function(query, compressed_data, centroids) {
   .Call('_BA23229020_pqSearchCpp', PACKAGE = 'BA23229020', query, compressed_data, centroids)
 }
